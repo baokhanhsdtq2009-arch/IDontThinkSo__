de1af25c4842f37282b9f6fe7ca1c4333a784f58
baokhanhsdtq2009-arch <baokhanhsdtq2009@gmail.com>
2026-09-02 10:49:39 +0700

    Refactor BaseState class formatting

--- blockblast/core/base_state.py
+++ blockblast/core/base_state.py
@@ -1,23 +1,15 @@
 class BaseState:
-
     def update(
         self
     ):
-
         pass
-
-
     def draw(
         self,
         screen
     ):
-
         pass
-
-
     def handle_event(
         self,
         event
     ):
-
         pass
